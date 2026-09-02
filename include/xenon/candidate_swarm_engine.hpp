e3756c13b77380c69aad7409e73bd5ab16cc6817
spiraletech <creeshchris@gmail.com>
2026-09-02 06:50:17 -0700

    feat(l22): add candidate swarm engine contract

---
+++ include/xenon/candidate_swarm_engine.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "xenon/candidate_pool.hpp"
+#include "xenon/candidate_ranker.hpp"
+#include "xenon/generation_pipeline.hpp"
+#include "xenon/media_analyzer.hpp"
+#include "xenon/synesthesia_scorer.hpp"
+#include "xenon/cortex_critic.hpp"
+
+#include <filesystem>
+#include <string>
+
+namespace xenon {
+
+class CandidateSwarmEngine {
+public:
+    explicit CandidateSwarmEngine(GenerationPipeline pipeline);
+
+    [[nodiscard]] CandidatePool generate_ranked(
+        const GenerationRequest& request,
+        const std::filesystem::path& output_directory,
+        const std::string& parent_fingerprint = {});
+
+private:
+    GenerationPipeline pipeline_;
+    MediaAnalyzer analyzer_;
+    SynesthesiaScorer synesthesia_;
+    CortexCritic critic_;
+    CandidateRanker ranker_;
+};
+
+} // namespace xenon
